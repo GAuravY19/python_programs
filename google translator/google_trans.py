178cb0df87724021d9cec42068625f585d7b09f0
gaurav <gauravy1905@gmail.com>
2023-06-10 20:46:11 +0530

    Translator commit

---
+++ google translator/google_trans.py
@@ -0,0 +1,26 @@
+from googletrans import Translator 
+from lang_list import LANGUAGES as lang
+
+def languages(dest):
+    for i,k in lang.items():
+        if dest == k:
+            dest = i 
+
+    return dest
+    
+def googleTrans():
+    translater = Translator()
+    word = translater.translate(text, dest)
+    print(word.text)
+    
+    
+print("Welcome to Personalised Google Translator.")
+print("Here You can translate as many words without using Internet.")
+text = input("Enter the text you want to translate :- ")
+dest = input("Enter the language of translation :- ")
+
+
+languages(dest)
+googleTrans()
+
+
